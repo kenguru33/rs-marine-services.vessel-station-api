4caa12bd37d7a923e41457291e22e8486a67c24e
Bernt Anker <bernt.anker@gmail.com>
2023-04-12 04:11:54 +0200

    Removed repository
    Removed repository. Only use service

--- src/stations/stations.module.ts
+++ src/stations/stations.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
 import { StationsController } from './stations.controller';
-import { StationsRepository } from './stations.repository';
 import { StationsService } from './stations.service';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [StationsService, StationsRepository],
+  providers: [StationsService],
   controllers: [StationsController],
 })
 export class StationsModule {}
--- src/stations/stations.service.ts
+++ src/stations/stations.service.ts
@@ -1,28 +1,63 @@
 import { Injectable } from '@nestjs/common';
 import { CreateStationDto } from './dto/createStation.dto';
 import { UpdateStationDto } from './dto/updateStation.dto';
-import { StationsRepository } from './stations.repository';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class StationsService {
-  constructor(private stationsRepository: StationsRepository) {}
+  constructor(private prisma: PrismaService) {}
 
   async station(id: number) {
-    return this.stationsRepository.station(id);
+    return this.prisma.station.findUnique({
+      where: { id },
+      include: {
+        vessels: {
+          include: { class: true, capabilities: true, state: true },
+        },
+      },
+    });
   }
   async stations() {
-    return this.stationsRepository.stations();
+    return this.prisma.station.findMany({
+      include: {
+        vessels: {
+          include: { class: true, capabilities: true, state: true },
+        },
+      },
+    });
   }
 
   async createStation(data: CreateStationDto) {
-    return this.stationsRepository.createStation(data);
+    return this.prisma.station.create({
+      data,
+      include: {
+        vessels: {
+          include: { class: true, capabilities: true, state: true },
+        },
+      },
+    });
   }
 
   async deleteStation(id: number) {
-    return this.stationsRepository.deleteStation(id);
+    return this.prisma.station.delete({
+      where: { id },
+      include: {
+        vessels: {
+          include: { class: true, capabilities: true, state: true },
+        },
+      },
+    });
   }
 
   async updateStation(id: number, data: UpdateStationDto) {
-    return this.stationsRepository.updateStation({ id, data });
+    return this.prisma.station.update({
+      where: { id },
+      data,
+      include: {
+        vessels: {
+          include: { class: true, capabilities: true, state: true },
+        },
+      },
+    });
   }
 }
