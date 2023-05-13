0e9147a79c30af2e115168505a7d2279ff7f94f3
Bernt Anker <bernt.anker@gmail.com>
2023-05-13 10:34:51 +0200

    work in progress

--- src/modules/vessel/vessel.module.ts
+++ src/modules/vessel/vessel.module.ts
@@ -15,29 +15,17 @@ import { VesselStateCategoryController } from './modules/vessel-state/vessel-sta
 import { VesselCapabilityModule } from './modules/vessel-capability/vessel-capability.module';
 import { VesselStateModule } from './modules/vessel-state/vessel-state.module';
 import { VesselCommunicationEquipmentModule } from './modules/vessel-communication-equipment/vessel-communication-equipment.module';
+import { VesselEngineModule } from './vessel-engine/vessel-engine.module';
 
 @Module({
-  providers: [
-    VesselService,
-    VesselTypeService,
-    VesselClassService,
-    //VesselCapabilityService,
-    // VesselStateService,
-    // VesselStateCategoryService
-  ],
-  controllers: [
-    VesselController,
-    VesselTypeController,
-    VesselClassController,
-    // VesselCapabilityController,
-    // VesselStateController,
-    // VesselStateCategoryController
-  ],
+  providers: [VesselService, VesselTypeService, VesselClassService],
+  controllers: [VesselController, VesselTypeController, VesselClassController],
   imports: [
     DatabaseModule,
     VesselCapabilityModule,
     VesselStateModule,
     VesselCommunicationEquipmentModule,
+    VesselEngineModule,
   ],
   exports: [],
 })
