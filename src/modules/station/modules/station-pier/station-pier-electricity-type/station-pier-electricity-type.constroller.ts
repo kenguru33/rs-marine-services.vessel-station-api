e45809313e3e8286524b5de74e10d5ceb5e6e6d8
Bernt Anker <bernt.anker@gmail.com>
2023-06-05 22:44:48 +0200

    pier electricty type

--- src/modules/station/modules/station-pier/station-pier-electricity-type/station-pier-electricity-type.constroller.ts
+++ src/modules/station/modules/station-pier/station-pier-electricity-type/station-pier-electricity-type.constroller.ts
@@ -1,8 +1,11 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
+  Post,
   Put,
   Query,
   UseInterceptors,
@@ -11,9 +14,11 @@ import { StationPierElectricityTypeService } from './station-pier-electricity-ty
 import { StationPierElectricityTypeResponseTransformInterceptor } from './interceptors/station-pier-electricity-type.interceptor';
 import { QueryStationPierElectricityTypeIncludeDto } from './dto/query-station-pier-electricity-type-include.dto';
 import { QueryStationPierElectricityTypeFilterDto } from './dto/query-station-pier-electricity-type-filter.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { QueryParamsValidatorInterceptor } from '../../../../../shared/interceptors/query-params-validator.interceptor';
 import { ALLOWED_FILTERS, ALLOWED_INCLUDES } from './constants';
+import { CreateStationPierElectricityTypeDto } from './dto/create-station-pier-electricity-type.dto';
+import { ResponseStationPierElectricityTypeDto } from './dto/response-station-pier-electricity-type.dto';
 
 @ApiTags('station-pier-electricity-type')
 @UseInterceptors(StationPierElectricityTypeResponseTransformInterceptor)
@@ -24,6 +29,7 @@ export class StationPierElectricityTypeController {
   ) {}
 
   @ApiOperation({ summary: 'Get all station pier electricity types' })
+  @ApiResponse({ type: ResponseStationPierElectricityTypeDto, isArray: true })
   @UseInterceptors(
     new QueryParamsValidatorInterceptor(ALLOWED_INCLUDES, ALLOWED_FILTERS),
   )
@@ -39,6 +45,7 @@ export class StationPierElectricityTypeController {
   }
 
   @ApiOperation({ summary: 'Get station pier electricity type by id' })
+  @ApiResponse({ type: ResponseStationPierElectricityTypeDto })
   @UseInterceptors(new QueryParamsValidatorInterceptor(ALLOWED_INCLUDES))
   @Get(':id')
   async getStationPierElectricityType(
@@ -51,6 +58,44 @@ export class StationPierElectricityTypeController {
     );
   }
 
-  
+  @ApiOperation({ summary: 'Create station pier electricity type' })
+  @ApiResponse({ type: ResponseStationPierElectricityTypeDto })
+  @UseInterceptors(new QueryParamsValidatorInterceptor(ALLOWED_INCLUDES))
+  @Post()
+  async createStationPierElectricityType(
+    @Body() data: CreateStationPierElectricityTypeDto,
+    @Query() queryInclude: QueryStationPierElectricityTypeIncludeDto,
+  ) {
+    return this.stationPierElectricityTypeService.createStationPierElectricityType(
+      data,
+      queryInclude,
+    );
+  }
 
+  @ApiOperation({ summary: 'Update station pier electricity type' })
+  @ApiResponse({ type: ResponseStationPierElectricityTypeDto })
+  @UseInterceptors(new QueryParamsValidatorInterceptor(ALLOWED_INCLUDES))
+  @Put(':id')
+  async updateStationPierElectricityType(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() data: CreateStationPierElectricityTypeDto,
+    @Query() queryInclude: QueryStationPierElectricityTypeIncludeDto,
+  ) {
+    return this.stationPierElectricityTypeService.updateStationPierElectricityType(
+      id,
+      data,
+      queryInclude,
+    );
+  }
+
+  @ApiOperation({ summary: 'Delete station pier electricity type' })
+  @ApiResponse({ type: ResponseStationPierElectricityTypeDto })
+  @Delete(':id')
+  async deleteStationPierElectricityType(
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.stationPierElectricityTypeService.deleteStationPierElectricityType(
+      id,
+    );
+  }
 }
