87df15c4ec6b0c6277af05e89620af2d06229472
Bernt Anker <bernt.anker@gmail.com>
2023-06-05 12:32:48 +0200

    electricy type

--- src/modules/station/modules/station-pier/station-pier-electricity-type/station-pier-electricity-type.constroller.ts
+++ src/modules/station/modules/station-pier/station-pier-electricity-type/station-pier-electricity-type.constroller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Put,
+  Query,
+  UseInterceptors,
+} from '@nestjs/common';
 import { StationPierElectricityTypeService } from './station-pier-electricity-type.service';
 import { StationPierElectricityTypeResponseTransformInterceptor } from './interceptors/station-pier-electricity-type.interceptor';
 import { QueryStationPierElectricityTypeIncludeDto } from './dto/query-station-pier-electricity-type-include.dto';
 import { QueryStationPierElectricityTypeFilterDto } from './dto/query-station-pier-electricity-type-filter.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { QueryParamsValidatorInterceptor } from '../../../../../shared/interceptors/query-params-validator.interceptor';
+import { ALLOWED_FILTERS, ALLOWED_INCLUDES } from './constants';
 
+@ApiTags('station-pier-electricity-type')
 @UseInterceptors(StationPierElectricityTypeResponseTransformInterceptor)
 @Controller('station-pier-electricity-type')
 export class StationPierElectricityTypeController {
@@ -11,6 +23,10 @@ export class StationPierElectricityTypeController {
     private stationPierElectricityTypeService: StationPierElectricityTypeService,
   ) {}
 
+  @ApiOperation({ summary: 'Get all station pier electricity types' })
+  @UseInterceptors(
+    new QueryParamsValidatorInterceptor(ALLOWED_INCLUDES, ALLOWED_FILTERS),
+  )
   @Get()
   async getStationPierElectricityTypes(
     @Query() queryInclude: QueryStationPierElectricityTypeIncludeDto,
@@ -21,4 +37,20 @@ export class StationPierElectricityTypeController {
       queryFilter,
     );
   }
+
+  @ApiOperation({ summary: 'Get station pier electricity type by id' })
+  @UseInterceptors(new QueryParamsValidatorInterceptor(ALLOWED_INCLUDES))
+  @Get(':id')
+  async getStationPierElectricityType(
+    @Param('id', ParseIntPipe) id: number,
+    @Query() queryInclude: QueryStationPierElectricityTypeIncludeDto,
+  ) {
+    return this.stationPierElectricityTypeService.getStationPierElectricityType(
+      id,
+      queryInclude,
+    );
+  }
+
+  
+
 }
